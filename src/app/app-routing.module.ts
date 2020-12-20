1d4c9abca34acaabea59358c48a13880ae3fd18a
jaskokoyn <jack@jaskokoyn.com>
2020-12-19 20:26:31 -0500

    update: Finished project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SecretComponent } from './secret/secret.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
-const routes: Routes = [];
+const authOnly = () => redirectUnauthorizedTo(['']);
+const guestOnly = () => redirectLoggedInTo(['secret']);
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: guestOnly }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: guestOnly }
+  },
+  {
+    path: 'secret',
+    component: SecretComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: authOnly }
+  },
+  { path: 'logout', component: LogoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/validators/compromised-password.ts
@@ -0,0 +1,25 @@
+import { AsyncValidator, FormControl } from '@angular/forms';
+import { EnzoicService } from '../enzoic.service';
+import { map, catchError } from 'rxjs/operators'
+import { of } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompromisedPassword implements AsyncValidator {
+  constructor(private enzoic: EnzoicService) { }
+
+  validate = (control: FormControl) => {
+    return this.enzoic.checkPassword(control.value).pipe(
+      map(() => {
+        return { compromised: true }
+      }),
+      catchError((err) => {
+        console.log(err);
+
+        return of(null);
+      })
+    );
+  }
+}
